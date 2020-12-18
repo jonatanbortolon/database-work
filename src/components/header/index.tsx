383b02672d124d953175621524176e6087739a2d
Jonatan Bortolon <jonatangabrielbortolon@outlook.com>
2020-12-18 09:08:39 -0300

    Minor fixes

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,35 +5,35 @@ const Header = () => {
     <header id="site-header" className="has-navigation-dropdown">
       <div className="header-wrapper secondary">
         <div className="portal-name">
-          <a href="/" className="portal-logo">
+          <a href="/#" className="portal-logo">
             <span className="sr-only">Portal Gov.br</span>
           </a>
         </div>
         <div className="site-header-links">
           <div className="links-rapidos">
-            <a className="toggle-links-rapidos" href="/" title="Acesso rápido">
+            <a className="toggle-links-rapidos" href="/#" title="Acesso rápido">
               <span className="fas fa-ellipsis-v"></span>
               <span className="sr-only">Acesso rápido</span>
             </a>
             <ul>
               <li className="titulo">Acesso rápido</li>
               <li>
-                <a href="/">Órgãos do Governo</a>
+                <a href="/#">Órgãos do Governo</a>
               </li>
               <li>
-                <a href="/">Acesso à Informação</a>
+                <a href="/#">Acesso à Informação</a>
               </li>
               <li>
-                <a href="/">Legislação</a>
+                <a href="/#">Legislação</a>
               </li>
               <li>
-                <a href="/">Acessibilidade</a>
+                <a href="/#">Acessibilidade</a>
               </li>
             </ul>
           </div>
           <ul className="header-icons">
             <li>
-              <a href="/" className="link-contraste">
+              <a href="/#" className="link-contraste">
                 <span className="fas fa-adjust" aria-hidden="true"></span>
                 <span className="sr-only">
                   Mudar para o modo de alto contraste
@@ -41,7 +41,7 @@ const Header = () => {
               </a>
             </li>
           </ul>
-          <a href="/" className="link-acesso">
+          <a href="/#" className="link-acesso">
             <span className="fas fa-user"></span>
             Entrar
           </a>
@@ -50,13 +50,13 @@ const Header = () => {
       <div className="main">
         <div className="header-wrapper">
           <div className="site-name-wrapper">
-            <a className="ico-navegacao toggle-main-navigation" href="/">
+            <a className="ico-navegacao toggle-main-navigation" href="/#">
               <span className="sr-only">Abrir menu principal de navegação</span>
               <span className="fa fa-bars" aria-hidden="true"></span>
               <span className="fa fa-times" aria-hidden="true"></span>
             </a>
             <div className="site-name">
-              <a href="/" title="O portal do Brasil">
+              <a href="/#" title="O portal do Brasil">
                 Governo do Brasil
               </a>
             </div>
@@ -91,54 +91,54 @@ const Header = () => {
           <div id="main-navigation" className="navigation-cell">
             <ul className="list-navigation">
               <li className="plain dropdown-submenu">
-                <a href="/" className="plain">
+                <a href="/#" className="plain">
                   Serviços
                 </a>
                 <ul className="submenu">
                   <li className="dropdown-submenu">
-                    <a href="/" className="state-protected hasDropDown">
+                    <a href="/#" className="state-protected hasDropDown">
                       Buscar serviços por
                     </a>
                     <ul className="submenu navTree navTreeLevel1">
                       <li>
-                        <a href="/" className="state-published">
+                        <a href="/#" className="state-published">
                           Categorias
                         </a>
                       </li>
                       <li>
-                        <a href="/" className="state-published">
+                        <a href="/#" className="state-published">
                           Órgãos
                         </a>
                       </li>
                       <li>
-                        <a href="/" className="state-published">
+                        <a href="/#" className="state-published">
                           Estados
                         </a>
                       </li>
                     </ul>
                   </li>
                   <li className="dropdown-submenu">
-                    <a href="/" className="state-protected hasDropDown">
+                    <a href="/#" className="state-protected hasDropDown">
                       Serviços por público alvo
                     </a>
                     <ul className="submenu navTree navTreeLevel1">
                       <li>
-                        <a href="/" className="state-published">
+                        <a href="/#" className="state-published">
                           Cidadãos
                         </a>
                       </li>
                       <li>
-                        <a href="/" className="state-published">
+                        <a href="/#" className="state-published">
                           Empresas
                         </a>
                       </li>
                       <li>
-                        <a href="/" className="state-published">
+                        <a href="/#" className="state-published">
                           Órgãos e Entidades Públicas
                         </a>
                       </li>
                       <li>
-                        <a href="/" className="state-published">
+                        <a href="/#" className="state-published">
                           Demais segmentos (ONGs, organizações sociais, etc)
                         </a>
                       </li>
@@ -147,182 +147,182 @@ const Header = () => {
                 </ul>
               </li>
               <li className="plain">
-                <a href="/" className="plain">
+                <a href="/#" className="plain">
                   Galeria de Aplicativos
                 </a>
               </li>
               <li className="plain dropdown-submenu">
-                <a href="/" className="plain">
+                <a href="/#" className="plain">
                   Notícias
                 </a>
                 <ul className="submenu">
                   <li>
-                    <a href="/" className="state-published">
+                    <a href="/#" className="state-published">
                       Últimas notícias
                     </a>
                   </li>
                   <li>
-                    <a href="/" className="state-published">
+                    <a href="/#" className="state-published">
                       Coronavirus
                     </a>
                   </li>
                   <li>
-                    <a href="/" className="state-published">
+                    <a href="/#" className="state-published">
                       Ações 2019
                     </a>
                   </li>
                   <li>
-                    <a href="/" className="state-published">
+                    <a href="/#" className="state-published">
                       Saúde
                     </a>
                   </li>
                   <li>
-                    <a href="/" className="state-published">
+                    <a href="/#" className="state-published">
                       Agricultura e Pecuária
                     </a>
                   </li>
                   <li className="dropdown-submenu">
-                    <a href="/" className="state-published hasDropDown">
+                    <a href="/#" className="state-published hasDropDown">
                       Cidadania e Assistência Social
                     </a>
                     <ul className="submenu navTree navTreeLevel1">
                       <li>
-                        <a href="/" className="state-published">
+                        <a href="/#" className="state-published">
                           Pátria Voluntária
                         </a>
                       </li>
                     </ul>
                   </li>
                   <li>
-                    <a href="/" className="state-published">
+                    <a href="/#" className="state-published">
                       Cultura e Esporte
                     </a>
                   </li>
                   <li className="dropdown-submenu">
-                    <a href="/" className="state-published hasDropDown">
+                    <a href="/#" className="state-published hasDropDown">
                       Economia e Gestão Pública
                     </a>
                     <ul className="submenu navTree navTreeLevel1">
                       <li>
-                        <a href="/" className="state-published">
+                        <a href="/#" className="state-published">
                           400 dias
                         </a>
                       </li>
                       <li>
-                        <a href="/" className="state-published">
+                        <a href="/#" className="state-published">
                           500 dias
                         </a>
                       </li>
                       <li>
-                        <a href="/" className="state-published">
+                        <a href="/#" className="state-published">
                           600 dias
                         </a>
                       </li>
                       <li>
-                        <a href="/" className="state-published">
+                        <a href="/#" className="state-published">
                           700 Dias
                         </a>
                       </li>
                     </ul>
                   </li>
                   <li>
-                    <a href="/" className="state-published">
+                    <a href="/#" className="state-published">
                       Educação e Pesquisa
                     </a>
                   </li>
                   <li>
-                    <a href="/" className="state-published">
+                    <a href="/#" className="state-published">
                       Energia
                     </a>
                   </li>
                   <li>
-                    <a href="/" className="state-published">
+                    <a href="/#" className="state-published">
                       Infraestrutura
                     </a>
                   </li>
                   <li>
-                    <a href="/" className="state-published">
+                    <a href="/#" className="state-published">
                       Justiça e Segurança
                     </a>
                   </li>
                   <li className="dropdown-submenu">
-                    <a href="/" className="state-published hasDropDown">
+                    <a href="/#" className="state-published hasDropDown">
                       Meio Ambiente
                     </a>
                     <ul className="submenu navTree navTreeLevel1">
                       <li>
-                        <a href="/" className="state-published">
+                        <a href="/#" className="state-published">
                           Operação Verde Brasil
                         </a>
                       </li>
                     </ul>
                   </li>
                   <li>
-                    <a href="/" className="state-published">
+                    <a href="/#" className="state-published">
                       Trabalho e Previdência
                     </a>
                   </li>
                   <li>
-                    <a href="/" className="state-published">
+                    <a href="/#" className="state-published">
                       Turismo
                     </a>
                   </li>
                 </ul>
               </li>
               <li className="plain">
-                <a href="/" className="plain">
+                <a href="/#" className="plain">
                   Acompanhe o Planalto
                 </a>
               </li>
               <li className="plain">
-                <a href="/" className="plain">
+                <a href="/#" className="plain">
                   Canais do Poder Executivo Federal
                 </a>
               </li>
               <li className="plain dropdown-submenu">
-                <a href="/" className="plain">
+                <a href="/#" className="plain">
                   Navegação
                 </a>
                 <ul className="submenu">
                   <li>
-                    <a href="/" className="state-published">
+                    <a href="/#" className="state-published">
                       Acessibilidade
                     </a>
                   </li>
                   <li>
-                    <a href="/" className="state-published">
+                    <a href="/#" className="state-published">
                       Mapa do Site
                     </a>
                   </li>
                   <li>
-                    <a href="/" className="state-published">
+                    <a href="/#" className="state-published">
                       Termos de Uso
                     </a>
                   </li>
                   <li>
-                    <a href="/" className="state-published">
+                    <a href="/#" className="state-published">
                       Sobre o gov.br
                     </a>
                   </li>
                 </ul>
               </li>
               <li className="plain">
-                <a href="/" className="plain">
+                <a href="/#" className="plain">
                   Acesse sua conta gov.br
                 </a>
               </li>
               <li className="plain">
-                <a href="/" className="plain">
+                <a href="/#" className="plain">
                   Guia de Edição de Serviços do Gov.br
                 </a>
               </li>
               <li className="plain">
-                <a href="/" className="plain">
+                <a href="/#" className="plain">
                   Órgãos do Governo
                 </a>
               </li>
               <li className="plain">
-                <a href="/" className="plain">
+                <a href="/#" className="plain">
                   Todos por todos
                 </a>
               </li>
@@ -334,10 +334,10 @@ const Header = () => {
             <div className="links-uteis links-uteis-mobile">
               <ul>
                 <li>
-                  <a href="/">Acessibilidade</a>
+                  <a href="/#">Acessibilidade</a>
                 </li>
                 <li>
-                  <a href="/" className="link-contraste">
+                  <a href="/#" className="link-contraste">
                     <span className="fas fa-adjust" aria-hidden="true"></span>
                     <span className="sr-only">
                       Mudar para o modo de alto contraste
@@ -350,31 +350,31 @@ const Header = () => {
               <div className="titulo">Links Úteis</div>
               <ul>
                 <li>
-                  <a href="/">Galeria de Aplicativos</a>
+                  <a href="/#">Galeria de Aplicativos</a>
                 </li>
                 <li>
-                  <a href="/">Participe</a>
+                  <a href="/#">Participe</a>
                 </li>
               </ul>
             </div>
             <div className="header-accessibility">
               <ul>
                 <li>
-                  <a href="/" className="orgaos-de-governo">
+                  <a href="/#" className="orgaos-de-governo">
                     Órgãos do Governo
                   </a>
                 </li>
                 <li>
-                  <a href="/">Galeria de Aplicativos</a>
+                  <a href="/#">Galeria de Aplicativos</a>
                 </li>
                 <li>
-                  <a href="/">Legislação</a>
+                  <a href="/#">Legislação</a>
                 </li>
                 <li>
-                  <a href="/">Acesso à Informação</a>
+                  <a href="/#">Acesso à Informação</a>
                 </li>
                 <li>
-                  <a href="/">Participe</a>
+                  <a href="/#">Participe</a>
                 </li>
               </ul>
             </div>
